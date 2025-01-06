9ba9a947cb8d626b274ced0bb9e24878d1e64f64
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-01-06 16:21:05 +0600

    first commit

---
+++ New folder/typesOfVariable.js
@@ -0,0 +1,14 @@
+var name = "nazmul";
+console.log(typeof name);
+
+var isShow = true;
+console.log(isShow);
+
+var isShow = false;
+console.log(isShow);
+
+var isShow = false;
+console.log(typeof isShow);
+
+var age = 22;
+console.log(typeof age);
