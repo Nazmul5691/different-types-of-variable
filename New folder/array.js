dbf44719b1b176f84d067bff21e797f1e9ae7999
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-01-06 19:27:54 +0600

    added concat

--- New folder/array.js
+++ New folder/array.js
@@ -5,18 +5,27 @@ friends[2] = 100;
 // console.log(friends);
 
 
-const place = ['cox bazar', 'sondarban', 'bandorban']
-place.push('madargong')
-place.push('ptengga', 'quakata')
+// const place = ['cox bazar', 'sondarban', 'bandorban']
+// place.push('madargong')
+// place.push('ptengga', 'quakata')
 // console.log(place);
-place.pop();
+// place.pop();
 // console.log(place);
 
+// const books = ['c++', 'python', 'java', 'javascript']
+// const available = books.includes('javascript')
+// if(available == true){
+//     console.log('Yes, the book is available');
+// }
+// else{
+//     console.log('Opps, Not available');
+// }
+
+
 const books = ['c++', 'python', 'java', 'javascript']
-const available = books.includes('javascript')
-if(available == true){
-    console.log('Yes, the book is available');
-}
-else{
-    console.log('Opps, Not available');
-}
+const place = ['cox bazar', 'sondarban', 'bandorban']
+const numbar = 20;
+const friend = true;
+
+
+console.log(books.concat(place));
