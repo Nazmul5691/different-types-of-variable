6b2c723fcfb17ba8b1d606f4647545a177c5d7fd
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-01-06 19:14:16 +0600

    added push pop

--- New folder/array.js
+++ New folder/array.js
@@ -1,5 +1,13 @@
 const friends = [10, 22, 40, 90, 66]
-console.log(friends[3]);
+// console.log(friends[3]);
 
 friends[2] = 100;
-console.log(friends);
+// console.log(friends);
+
+
+const place = ['cox bazar', 'sondarban', 'bandorban']
+place.push('madargong')
+place.push('ptengga', 'quakata')
+console.log(place);
+place.pop();
+console.log(place);
